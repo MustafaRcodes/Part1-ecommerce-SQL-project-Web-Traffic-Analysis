5f451493d8dc34aa1c2cd4a58b7221505d3aec96
MustafaR_Projects <150495517+MustafaRcodes@users.noreply.github.com>
2024-07-04 11:16:01 -0400

    Add files via upload

--- Ecommerce and web analytics.sql
+++ Ecommerce and web analytics.sql
@@ -883,4 +883,93 @@ GROUP BY
     billing_version_seen;
     
 -- Looks like the new version of the billing page has done much better job converting customers
--- will keep goig with this strategy and roll out to all of our customer right away. Great potential fro revenue increase. 
+-- will keep goig with this strategy and roll out to all of our customer right away. Great potential fro revenue increase.
+/*
+1. Gsearch seems to be the biggest driver of business. Pulling monthly trends for Gsearch sessions 
+and orders so that we can showcase growth here. 
+*/
+
+SELECT 
+  YEAR(website_sessions.created_at) AS yr,
+  MONTH(website_sessions.created_at) AS mo,
+  COUNT(DISTINCT website_sessions.website_session_id) AS sessions,
+  COUNT(DISTINCT orders.order_id) AS orders,
+  COUNT(DISTINCT orders.order_id)/COUNT(DISTINCT website_sessions.website_session_id) AS conv_rate
+FROM website_sessions
+  LEFT JOIN orders
+    ON orders.website_session_id = website_sessions.website_session_id
+WHERE website_sessions.created_at < '2012-11-27'
+    AND website_sessions.utm_source = 'gsearch'
+GROUP BY 1,2;
+
+/*
+2. Next, it would be great to see a similar trend for Gsearch, but this time splitting out nonbrand and brand
+campaign separately.
+*/
+
+SELECT 
+  YEAR(website_sessions.created_at) AS yr,
+  MONTH(website_sessions.created_at) AS mo,
+  COUNT(DISTINCT CASE WHEN utm_campaign = 'nonbrand' THEN website_sessions.website_session_id ELSE NULL END) AS nonbrand_sessions,
+  COUNT(DISTINCT CASE WHEN utm_campaign = 'nonbrand' THEN orders.order_id ELSE NULL END) AS nonbrand_orders,
+  COUNT(DISTINCT CASE WHEN utm_campaign = 'brand' THEN website_sessions.website_session_id ELSE NULL END) AS brand_sessions,
+  COUNT(DISTINCT CASE WHEN utm_campaign = 'brand' THEN orders.order_id ELSE NULL END) AS brand_orders
+FROM 
+  website_sessions
+LEFT JOIN orders
+   ON orders.website_session_id = website_sessions.website_session_id
+WHERE website_sessions.created_at < '2012-11-27'
+   AND website_sessions.utm_source = 'gsearch'
+GROUP BY 1,2;
+
+/*
+3. While we're on Gsearch, we will dive into nonbrand, and pull monthly sessions and orders split by 
+device type. 
+*/
+
+SELECT
+   YEAR(website_sessions.created_at) AS yr,
+   MONTH(website_sessions.created_at) AS mo,
+   COUNT(DISTINCT CASE WHEN device_type = 'desktop' THEN website_sessions.website_session_id ELSE NULL END) AS desktop_sessions,
+   COUNT(DISTINCT CASE WHEN device_type = 'desktop' THEN orders.order_id ELSE NULL END) AS desktop_orders,
+   COUNT(DISTINCT CASE WHEN device_type = 'mobile' THEN website_sessions.website_session_id ELSE NULL END) AS mobile_sessions,
+   COUNT(DISTINCT CASE WHEN device_type = 'mobile' THEN orders.order_id ELSE NULL END) AS mobile_orders
+FROM website_sessions
+    LEFT JOIN orders
+      ON orders.website_session_id = website_sessions.website_session_id
+WHERE website_sessions.created_at < '2012-11-27'
+   AND website_sessions.utm_source = 'gsearch'
+   AND website_sessions.utm_campaign = 'nonbrand'
+GROUP BY 1,2;
+
+/* concerned about the large % of traffic is from Gsearch. Pulling monthly trends for Gsearch, 
+alongside monthly trend for each of our other channel.
+*/
+
+SELECT DISTINCT 
+  utm_source,
+  utm_campaign,
+  http_referer
+FROM website_sessions
+WHERE website_sessions.created_at < '2012-11-27';
+
+SELECT
+   YEAR(website_sessions.created_at) AS yr,
+   MONTH(website_sessions.created_at) AS mo,
+   COUNT(DISTINCT CASE WHEN utm_source = 'gsearch' THEN website_sessions.website_session_id ELSE NULL END) AS gsearch_paid_sessions,
+   COUNT(DISTINCT CASE WHEN utm_source = 'bsearch' THEN website_sessions.website_session_id ELSE NULL END) AS bsearch_paid_sessions,
+   COUNT(DISTINCT CASE WHEN utm_source IS NULL  AND http_referer IS NOT NULL THEN website_sessions.website_session_id ELSE NULL END) AS organic_search_sessions,
+   COUNT(DISTINCT CASE WHEN utm_source IS NULL  AND http_referer IS NULL THEN website_sessions.website_session_id ELSE NULL END) AS direct_type_in_sessions
+FROM website_sessions
+    LEFT JOIN orders 
+      ON orders.website_session_id = website_sessions.website_session_id
+WHERE website_sessions.created_at < '2012-11-27'
+GROUP BY 1,2;
+
+/*
+5. Checking the story of our website performance improvement over the course of the first 8 months.
+pulling session to order conversion rates by month.
+*/
+
+
+    
