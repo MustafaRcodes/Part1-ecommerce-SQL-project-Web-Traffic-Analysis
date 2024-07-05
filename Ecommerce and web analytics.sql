3ec458b74679df6dabc6f85de8a6b83c1cc79ccd
MustafaR_Projects <150495517+MustafaRcodes@users.noreply.github.com>
2024-07-05 13:23:44 -0400

    Add files via upload

--- Ecommerce and web analytics.sql
+++ Ecommerce and web analytics.sql
@@ -1150,3 +1150,60 @@ GROUP BY
     
 -- Looks like not only are our brand , direct an organic volumes growing but are growing as a percentage 
 -- of our paid traffic volume.
+
+-- ANALYZING SEASONALITY AND BUSINESS PARTNERS
+-- Analyzing business patterns is about generating insights to maximize efficiency and anticipate future trend.alter
+-- USING SQL date functions here.
+
+-- We should take a look at 2012 monthly and weekly volume patters to see if we can find any seasonal trend 
+-- we should plan for in 2024
+-- we can pull session volume and order volume. 
+
+SELECT 
+    YEAR(website_sessions.created_at) AS yr,
+    WEEK(website_sessions.created_at) AS wk,
+    MIN(DATE(website_sessions.created_at)) AS week_start,
+    COUNT(DISTINCT website_sessions.website_session_id) AS sessions,
+    COUNT(DISTINCT orders.order_id) AS orders
+FROM 
+    website_sessions
+     LEFT JOIN orders
+       ON website_sessions.website_session_id = orders.website_session_id
+WHERE website_sessions.created_at < '2013-01-01'
+GROUP BY 1,2;
+
+-- Looks like we grew fairly steadily all year and saw significant volume around the holiday month 
+-- (especially the week of black friday and cyber monday)
+-- We want to keep in mind in 2013 as we think about customer support and inventory management.
+
+-- Now we will analyze the average website session volume by hour of day and by day week so that we can staff appropriately.
+-- Lets avoid the holiday time period and use a date range of sep 15 - nov 12 2012.
+
+
+SELECT 
+    hr,
+    ROUND(AVG(website_sessions),1) AS avg_sessions,
+    ROUND(AVG(CASE WHEN wkday = 0 THEN website_sessions ELSE NULL END),1) AS mon,
+    ROUND(AVG(CASE WHEN wkday = 1 THEN website_sessions ELSE NULL END),1) AS tue,
+    ROUND(AVG(CASE WHEN wkday = 2 THEN website_sessions ELSE NULL END),1) AS wed,
+    ROUND(AVG(CASE WHEN wkday = 3 THEN website_sessions ELSE NULL END),1) AS thur,
+    ROUND(AVG(CASE WHEN wkday = 4 THEN website_sessions ELSE NULL END),1) AS fri,
+    ROUND(AVG(CASE WHEN wkday = 5 THEN website_sessions ELSE NULL END),1) AS sat,
+    ROUND(AVG(CASE WHEN wkday = 6 THEN website_sessions ELSE NULL END),1) AS sun
+FROM(
+SELECT 
+    DATE(created_at) AS created_date,
+    WEEKDAY(created_at) AS wkday,
+    HOUR(created_at) AS hr,
+    COUNT(DISTINCT website_session_id) AS website_sessions
+FROM 
+    website_sessions
+WHERE created_at BETWEEN '2012-09-15' AND '2012-11-15'
+GROUP BY 1,2,3
+) AS daily_hourly_sessions
+GROUP BY 1
+ORDER BY 1
+
+-- Looks like 10 sessions per hour per employee staffed is about right 
+-- Looks like then we can plan on one support staff around the clock and then we should double up to 
+-- two staff member from 8 am to 5 pm MOnday through friday.
